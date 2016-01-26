8b2f67b44ae705744ec81c1d249ff37e6f06ccfa
abbasgh12345 <abbasgh91@gmail.com>
2016-01-26 13:26:30 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -204,6 +204,9 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
+    "plugin",
+    "echo",
+    "id",
     "onservice",
     "inrealm",
     "ingroup",
@@ -221,7 +224,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {179983320},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
